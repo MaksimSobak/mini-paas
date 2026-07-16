9e0e26cfa59325b9c9c40b90f814829cb97040c1
MaksimSobak <emelanovmaksim912@gmail.com>
2026-07-16 16:38:18 +0500

    Add frontend clone request logic

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,33 +1,58 @@
-const button = document.getElementById("folderButton");
-const folderName = document.getElementById("folderName");
-
-button.addEventListener("click", async () => {
-
-    if (!window.showDirectoryPicker) {
-        folderName.textContent =
-            "Your browser doesn't support folder selection.";
-        return;
-    }
-
-    try {
-        const dir = await window.showDirectoryPicker();
-        folderName.textContent = `Selected: ${dir.name}`;
-    } catch {
-        folderName.textContent = "Folder selection cancelled.";
-    }
-
-});
-
+const urlInput = document.getElementById("url");
+const folderInput = document.getElementById("folder");
 const cloneButton = document.getElementById("cloneButton");
+const status = document.getElementById("status");
+
 
 cloneButton.addEventListener("click", async () => {
 
-    const response = await fetch("/clone", {
-        method: "POST"
-    });
+    const repoUrl = urlInput.value.trim();
+    const folder = folderInput.value.trim();
 
-    const data = await response.json();
 
-    alert(data.message);
+    if (!repoUrl) {
+        alert("Enter GitHub repository URL");
+        return;
+    }
+
+
+    if (!folder) {
+        alert("Enter destination folder");
+        return;
+    }
+
+
+    try {
+
+        const response = await fetch("/clone", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                url: repoUrl,
+                folder: folder
+            })
+        });
+
+
+        const data = await response.json();
+
+        status.textContent = data.message;
+
+
+    } catch(error) {
+
+        console.error(error);
+
+        status.textContent =
+            "Server error";
+    }
 
 });
+
+console.log(urlInput);
+console.log(folderInput);
+console.log(cloneButton);
+console.log(status);
