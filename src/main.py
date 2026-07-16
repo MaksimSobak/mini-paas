7593d6e6b4eb54f91dbccfbbd7db4f80e139cd72
MaksimSobak <emelanovmaksim912@gmail.com>
2026-07-16 16:38:23 +0500

    Add clone API endpoint

--- src/main.py
+++ src/main.py
@@ -1,35 +1,73 @@
 import subprocess
+import os
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
-templates = Jinja2Templates(directory="src/templates")
+app.mount(
+    "/static",
+    StaticFiles(directory="src/static"),
+    name="static"
+)
+
+
+templates = Jinja2Templates(
+    directory="src/templates"
+)
+
+
+class CloneRequest(BaseModel):
+    url: str
+    folder: str
+
 
 
 @app.get("/")
 async def home(request: Request):
+
     return templates.TemplateResponse(
         request=request,
-        name="index.html",
+        name="index.html"
     )
+
 
 
 @app.post("/clone")
-async def clone():
+async def clone(data: CloneRequest):
 
-    subprocess.run(
-        ["bash", "scripts/clone.sh"],
-        check=True,
-    )
+    try:
 
-    return JSONResponse(
-        {
-            "message": "Clone script executed successfully."
+        subprocess.run(
+            [
+                "bash",
+                "scripts/clone.sh",
+                data.url,
+                data.folder
+            ],
+            check=True
+        )
+
+
+        return {
+            "message":
+            f"Repository cloned into {data.folder}"
         }
-    )
+
+
+    except subprocess.CalledProcessError:
+
+        return JSONResponse(
+            {
+                "message":
+                "Clone failed"
+            },
+            status_code=500
+        )
