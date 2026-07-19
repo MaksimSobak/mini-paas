84525575e4e56a5ec96811780c6dcd8c002f5f87
MaksimSobak <emelanovmaksim912@gmail.com>
2026-07-19 13:07:45 +0500

    Updated the main code to work with multiple bash scripts

--- src/main.py
+++ src/main.py
@@ -1,73 +1,96 @@
 import subprocess
 import os
+import uuid
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
-
 app.mount(
     "/static",
     StaticFiles(directory="src/static"),
-    name="static"
+    name="static",
 )
 
-
-templates = Jinja2Templates(
-    directory="src/templates"
-)
+templates = Jinja2Templates(directory="src/templates")
 
 
-class CloneRequest(BaseModel):
+class DeployRequest(BaseModel):
     url: str
     folder: str
 
 
-
 @app.get("/")
 async def home(request: Request):
-
     return templates.TemplateResponse(
         request=request,
-        name="index.html"
+        name="index.html",
     )
 
 
-
-@app.post("/clone")
-async def clone(data: CloneRequest):
-
+@app.post("/deploy")
+async def deploy(data: DeployRequest):
     try:
+        project_path = os.path.join("repositories", data.folder)
 
+        image_name = f"image-{uuid.uuid4().hex[:8]}"
+        container_name = f"container-{uuid.uuid4().hex[:8]}"
+
+        # Клонирование репозитория
         subprocess.run(
             [
                 "bash",
                 "scripts/clone.sh",
                 data.url,
-                data.folder
+                project_path,
             ],
-            check=True
+            check=True,
         )
 
+        # Сборка Docker-образа
+        subprocess.run(
+            [
+                "bash",
+                "scripts/build.sh",
+                project_path,
+                image_name,
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+
+        # Запуск контейнера
+        subprocess.run(
+            [
+                "bash",
+                "scripts/run.sh",
+                image_name,
+                container_name,
+            ],
+            check=True,
+        )
 
         return {
-            "message":
-            f"Repository cloned into {data.folder}"
+            "message": "Application deployed successfully.",
+            "image": image_name,
+            "container": container_name,
         }
 
-
-    except subprocess.CalledProcessError:
-
+    except subprocess.CalledProcessError as e:
         return JSONResponse(
-            {
-                "message":
-                "Clone failed"
+            status_code=500,
+            content={
+                "stdout": e.stdout,
+                "stderr": e.stderr,
             },
-            status_code=500
         )
+    
+    print(project_path)
+
+    
